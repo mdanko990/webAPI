72c4f0f3177853882777f45b73c921d171970eab
mdanko990 <62939304+mdanko990@users.noreply.github.com>
2021-05-24 12:36:43 +0300

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -1,92 +1,99 @@
-const { step } = require('mocha-steps');
 var assert = require('assert');
 const {done} = require('mocha');
 var axios = require('axios').default;
-var FormData = require('form-data');
+
 const token = "eyJ0eXAiOiAiVENWMiJ9.dlZvMVFtUVRJQWkwYkQ1SmFLVVc3MEtpbnI0.Y2RmNDY1OTgtNTI0MS00NWVhLWI0NzgtNGZjN2I0YjMzMzM2";
 const filePath = "../file.txt";
 var fileId;
 
-describe("WebAPI", ()=>{
-    
-    it("Upload file test", (done)=>{
-        const uploadUrl = "https://content.dropboxapi.com/2/files/upload";
-        axios({
-            method: 'post',
-            url: uploadUrl,
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Dropbox-API-Arg': JSON.stringify({
-                    'path': '../file.txt',
-                    'mode': 'add',
-                    'autorename': true,
-                    'mute': false,
-                    'strict_conflict': false
-                }),
-                'Content-Type': 'application/octet-stream'
-            },
-            data: {
-                binary: filePath
-            }
-            
-        })
-        .then((response) =>{
-            fileId = response.data.id;
-            assert.strictEqual(response.status, 200);
-            done();            
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    });
 
-    it("Get metadata test", (done)=>{
-        const metadataUrl = "https://api.dropboxapi.com/2/files/get_metadata";
-        axios({      
-            method: 'post',      
-            url: metadataUrl,
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json',
-                
-            },
-            data: JSON.stringify({
-                'path': '../file.txt',
-                'include_media_info': true
-            })
+class webAPI{
+  uploadFile(userData){
+    try{
+      const response = axios.post('https://content.dropboxapi.com/2/files/upload', {
+        headers: userData.getHeaders,
+        data: {
+            binary: userData.filePath
+        }
+      });
+      userData.fileId = response.data.id;
+      assert.strictEqual(response.status, 200);
+      done();
+    }catch(err){
+      console.log(err);
+    }
+  }
+  getMetadata(userData){
+    try{
+      const response = axios.post('https://api.dropboxapi.com/2/files/get_metadata', {
+        headers: userData.getHeaders,
+        data: JSON.stringify({
+            'path': userData.filePath,
+            'include_media_info': true
         })
-        .then((response) =>{
-            recievedId = response.data.id;
-            assert.strictEqual(recievedId, fileId);            
-            done();
+      });
+      recievedId = response.data.id;
+      assert.strictEqual(recievedId, userData.fileId);            
+      done();
+    }catch(err){
+      console.log(err);
+    }
+  }
+  deleteFile(userData){
+    try{
+      const response = axios.post('https://api.dropboxapi.com/2/files/delete_v2', {
+        headers: userData.headers,
+        data: JSON.stringify({
+            'path': userData.filePath
         })
-        .catch((err) => {
-            console.log(err);
-        });
-    });
+      });
+      assert.strictEqual(response.status, 200);          
+      done();
+    }catch(err){
+      console.log(err);
+    }
+  }
+}
 
-    it("Delete file test", (done)=>{
-        const metadataUrl = "https://api.dropboxapi.com/2/files/delete_v2";
-        axios({      
-            method: 'post',      
-            url: metadataUrl,
-            headers: {
-                'Authorization': 'Bearer ' + token,
-                'Content-Type': 'application/json',
-                
-            },
-            data: JSON.stringify({
-                'path': '../file.txt'
-            })
-        })
-        .then((response) =>{
-            assert.strictEqual(response.status, 200);          
-            done();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    });
-    
-    
+class userData{
+  constructor(token, filePath, fileId = ''){
+    this.token = token;
+    this.filePath = filePath;
+    this.fileId = fileId;
+  }
+  set setHeaders(data){
+    this.headers = data;
+  }
+  get getHeaders(){
+    return JSON.parse(this.setHeaders);
+  }
+}
+
+let uploadTestData = new userData(token, filePath);
+let getTestData = new userData(token, filePath);
+let deleteTestData = new userData(token, filePath);
+
+uploadTestData.headers = JSON.stringify({
+  'Authorization': 'Bearer ' + this.token,
+  'Dropbox-API-Arg': JSON.stringify({
+      'path': this.filePath,
+      'mode': 'add',
+      'autorename': true,
+      'mute': false,
+      'strict_conflict': false
+  }),
+  'Content-Type': 'application/octet-stream'
 });
+getTestData.headers = JSON.stringify({
+  'Authorization': 'Bearer ' + token,
+  'Content-Type': 'application/json',
+});
+deleteTestData.headers = JSON.stringify({
+  'Authorization': 'Bearer ' + token,
+  'Content-Type': 'application/json',
+});
+
+let tests = new webAPI();
+tests.uploadFile(uploadTestData);
+tests.getMetadata(getTestData);
+tests.deleteFile(deleteTestData);
